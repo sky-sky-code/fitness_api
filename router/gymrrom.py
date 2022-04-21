2fe882a691223fba3c76808667006354a6a8e40d
sky-sky-code <ps@praesens.ru>
2022-04-21 12:09:13 +0300

    Изменение response body у gymroom

--- router/gymrrom.py
+++ router/gymrrom.py
@@ -24,24 +24,24 @@ async def get_one_gymroom(uid: uuid.UUID):
     return await GymRoom_Pydantic.from_queryset(get_gym)
 
 
-@router.put('/gymroom/{uid:uuid}', response_model=GymRoom_Raw_Pydantic)
+@router.put('/gymroom/{uid:uuid}', response_model=GymRoom_Pydantic)
 async def update_gymroom(uid: uuid.UUID, gymroom: GymRoom_Raw_Pydantic):
     get_gym = await models.GymRoom.get(uid=uid)
     update_gym = await get_gym.update_from_dict(gymroom.dict())
     await update_gym.save()
-    return await GymRoom_Raw_Pydantic.from_tortoise_orm(update_gym)
+    return await GymRoom_Pydantic.from_tortoise_orm(update_gym)
 
 
-@router.delete('/gymroom/{uid:uuid}', response_model=GymRoom_Raw_Pydantic)
+@router.delete('/gymroom/{uid:uuid}')
 async def delete_gym(uid: uuid.UUID):
     get_gym = await models.GymRoom.get(uid=uid)
     await get_gym.delete()
-    return {'msg': "GymRoom delete successful"}
+    return {'msg': f"GymRoom {get_gym.name} delete successful"}
 
 
-@router.post('/gymroom', response_model=GymRoom_Raw_Pydantic)
+@router.post('/gymroom', response_model=GymRoom_Pydantic)
 async def gymroom_create(gymroom: GymRoom_Raw_Pydantic):
-    get_office = models.Office.get(uid=gymroom.office)
-    gymroom.office = get_office
+    get_office = await models.Office.get(uid=gymroom.office_id)
+    gymroom.office_id = get_office.uid
     create_gymroom = await models.GymRoom.create(**gymroom.dict())
-    return await GymRoom_Raw_Pydantic.from_tortoise_orm(create_gymroom)
+    return await GymRoom_Pydantic.from_tortoise_orm(create_gymroom)
