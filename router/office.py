3dd0e12a67d7e334939602a47871cc8a2ccc3b84
sky-sky-code <ps@praesens.ru>
2022-04-21 11:39:06 +0300

    Изменение resposne body у office

--- router/office.py
+++ router/office.py
@@ -24,24 +24,24 @@ async def get_one_office(uid: uuid.UUID):
     return await Office_Pydantic.from_tortoise_orm(get_office)
 
 
-@router.post('/office', response_model=Office_Raw_Pydantic)
+@router.post('/office', response_model=Office_Pydantic)
 async def add_office(office: Office_Raw_Pydantic):
     new_office = office.dict()
     new_office['uid'] = uuid.uuid4()
     create_doc = await models.Office.create(**new_office)
-    return await Office_Raw_Pydantic.from_tortoise_orm(create_doc)
+    return await Office_Pydantic.from_tortoise_orm(create_doc)
 
 
-@router.put('/office/{uid:uuid}', response_model=Office_Raw_Pydantic)
+@router.put('/office/{uid:uuid}', response_model=Office_Pydantic)
 async def update_office(uid: uuid.UUID, office: Office_Raw_Pydantic):
     get_office = await models.Office.get(uid=uid)
     get_office = await get_office.update_from_dict(office.dict())
     await get_office.save()
-    return await Office_Raw_Pydantic.from_tortoise_orm(get_office)
+    return await Office_Pydantic.from_tortoise_orm(get_office)
 
 
 @router.delete('/office/{uid:uuid}')
 async def del_office(uid: uuid.UUID):
     get_office = await models.Office.get(uid=uid)
     await get_office.delete()
-    return {'msg': "Office delete successful"}
+    return {'msg': f"Office {get_office.name} delete successful"}
