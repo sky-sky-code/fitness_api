b3da7404be1487de6a4d497721935246afc26c4c
sky-sky-code <ps@praesens.ru>
2022-05-13 23:03:32 +0300

    Правки

--- router/client.py
+++ router/client.py
@@ -12,7 +12,7 @@ router = APIRouter(
 
 @router.get('/client', response_model=List[Profile_Pydantic])
 async def get_client():
-    clients = models.User.all()
+    clients = models.Profile.all()
     return await Profile_Pydantic.from_queryset(clients)
 
 
@@ -20,19 +20,19 @@ async def get_client():
 async def create_client(data_client: Profile_Raw_Pydantic):
     data_client = data_client.dict()
     data_client['uid'] = uuid.uuid4()
-    client = await models.User.create(**data_client)
+    client = await models.Profile.create(**data_client)
     return await Profile_Pydantic.from_tortoise_orm(client)
 
 
 @router.get('/client/{uid:uuid}', response_model=Profile_Pydantic)
 async def get_one_client(uid: uuid.UUID):
-    client = await models.User.get(uid=uid)
+    client = await models.Profile.get(uid=uid)
     return await Profile_Pydantic.from_tortoise_orm(client)
 
 
 @router.put("/client/{uid:uuid}", response_model=Profile_Pydantic)
 async def update_client(uid: uuid.UUID, data_client: Profile_Raw_Pydantic):
-    user = await models.User.get(uid=uid)
+    user = await models.Profile.get(uid=uid)
     client = await user.update_from_dict(data_client.dict())
     await client.save()
     return await Profile_Pydantic.from_tortoise_orm(client)
@@ -40,6 +40,6 @@ async def update_client(uid: uuid.UUID, data_client: Profile_Raw_Pydantic):
 
 @router.delete("/client/{uid:uuid}")
 async def delete_client(uid: uuid.UUID):
-    get_client = await models.User.get(uid=uid)
+    get_client = await models.Profile.get(uid=uid)
     await get_client.delete()
     return {'msg': f'Delete {get_client.name} {get_client.pat_name} {get_client.surname} client successful'}
